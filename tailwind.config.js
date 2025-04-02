5c4d45718f3aa2bab5a1debe804ac3e9aa49c9aa
Thomas-pgrm <141674293+Thomas-pgrm@users.noreply.github.com>
2025-04-02 12:37:30 +0200

    Instalation de l'environnement Tailwinds CSS

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./Vue/Components/**/*.html"], // ✅ Scan tous les fichiers HTML dans /Vue/Components
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
